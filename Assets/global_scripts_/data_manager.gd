0cf301610aff37e5c930131adbd093830d9186fc
Autolyco <autolycostudio@gmail.com>
2021-07-06 15:51:21 +0200

    working on save prototype

---
+++ Assets/global_scripts_/data_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+
+
+# Global scripts who handle all data from the player and other nodes related to it
+# There is a save and load function 
+
+
+
+
+func _ready():
+	print("data handler initialized")
+
+
+func save_data():
+	pass
+
+
+func load_data():
+	pass
+
+
+
+
